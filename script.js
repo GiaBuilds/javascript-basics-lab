f64c784ea634f9e9f78ab672d3d92e5115214ae9
GiaBuilds <giaturnerrr@gmail.com>
2024-09-20 23:45:54 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+var studentName ="Gia"
+let studentAge ="21"
+let isEnrolled ="true"
+
+console.log(studentName)
+console.log(studentAge)
+console.log(isEnrolled)
+
+let lastName = "Turner"
+let distance = 10.5;
+let found = true;
+let colors = ['Pink', 'Green', 'Red'];
+
+const person = new Object ();
+person .age = 25;
+
+console.log(lastName)
+console.log(distance)
+console.log(found)
+console.log(person.age)
+
+let a = 6
+let b = 3
+console.log (a + b)
+
+let y = 12
+let x = 14
+console.log ("12 == y");
+console.log ("y < x");
+console.log ("x != y");
+
+let c = true;
+let d= false;
+console.log(c && d); // Logical AND
+console.log(c || d); // Logical OR
+console.log(!c); // Logical NOT
